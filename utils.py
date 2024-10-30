24244edb398c5e6e8d567e54ea3e98bf3b6e265d
Helen <helen@climatenode.org>
2024-10-30 17:34:16 +0000

    Forgot to add utils.py to previous commit

---
+++ utils.py
@@ -0,0 +1,21 @@
+from typing import Callable
+from typing import List
+from typing import Tuple
+from typing import Union
+
+
+def first(seq: Union[List, Tuple], lambda_: Callable) -> object:
+    """Return first element of seq that returns a positive result from the condition specified in lambda_"""
+    return next((ele for ele in seq if lambda_(ele)), None)
+
+
+def unique(seq: Union[List, Tuple]) -> List:
+    """Find unique elements of a sequence and retain order"""
+    seen = {}
+    result = []
+    for item in seq:
+        if item in seen:
+            continue
+        seen[item] = 1
+        result.append(item)
+    return result
