79cc4ea71edd7c40cf4ad32b67439e71dc240c05
Helen <helen@climatenode.org>
2025-01-14 13:57:46 +0000

    Can now interact with Google API

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 # DB_PATH = "retrieval/db/ccid_demo_db"
 DB_PATH = "retrieval/db/full_site_demo_db_with_pdfs"
+GOOGLE_API_CREDENTIALS_PATH = "google-api/credentials.json"
+
 DEFAULT_MODEL = "gpt-4o-mini"
 DEFAULT_EMBEDDINGS_MODEL = "text-embedding-3-small"
 RPM_RATE_LIMIT = 10000
 TPM_RATE_LIMIT = 5e6
+
 EARLIEST_YEAR = 2003  # 2003 is the earliest publication date in the DB
 DEFAULT_START_YEAR = 2019
