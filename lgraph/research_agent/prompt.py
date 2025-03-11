f771b55d76fb2d5355bcb348e78183dd46cf458b
Helen <helen@climatenode.org>
2025-03-11 16:38:55 +0000

    First experiment with simple reserch agent

---
+++ lgraph/research_agent/prompt.py
@@ -0,0 +1,118 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from langchain_core.prompts import PromptTemplate
+from lgraph.graph import State
+
+
+if TYPE_CHECKING:
+    pass
+
+
+today = datetime.now().strftime("%d %B %Y")
+
+my_office_template = """
+Project Proposal Template
+
+A project proposal should have three components:
+
+A problem statement: a description of the problem you are trying to solve
+Method: a description of how you will do it
+Partners: which external organisations you will work with
+"""
+
+
+write_template = f"""
+    You are a helpful assistant and an expert on the internal administration, personnel and projects of the innovation agency Nesta.
+
+    Your job is to apply the given template to structure the output of the request.
+
+    REQUEST:
+    {{request}}
+
+
+    TEMPLATE:
+    {my_office_template}
+
+"""  # noqa
+
+write_prompt = PromptTemplate(template=write_template, input_variables=["request"])
+
+
+# copied/adapted from GPT Researcher
+
+
+edit_prompt_template = f"""
+            Your task is to generate an outline of sections headers for the document based on the template and user request below.
+            You must return nothing but a JSON with the fields 'title' (str) and
+            'sections' with the following structure:
+            '{{title: string research title, date: {today},
+            sections: ['section header 1', 'section header 2', 'section header 3' ...]}}'.
+
+            TEMPLATE:
+            {{template}}
+
+            USER REQUEST:
+            {{request}}
+            """
+
+edit_prompt = PromptTemplate(template=edit_prompt_template, input_variables=["request", "template"])
+
+revision_format = """
+{{
+  "draft": The revised draft that you are submitting for review
+  "revision_notes": Your message to the reviewer about the changes you made to the draft based on their feedback
+}}
+"""
+
+
+revision_template = f"""
+You have been tasked by your reviewer with revising the following draft.
+Read the reviewer's notes, then write a new draft and make sure to address all of the points they raised.
+Please keep all other aspects of the draft the same.
+You MUST return nothing but a JSON in the following format:
+{revision_format}
+\nDraft:\n{{draft}}"\n\n"Reviewer's notes:\n{{critique}}\n\n
+"""
+
+revision_prompt = PromptTemplate(template=revision_template, input_variables=["draft", "critique"])
+
+
+reviser_addendum = """\nThe reviser has already revised the draft based on your previous review notes with the following feedback:
+{revision_notes}\n
+Please provide additional feedback ONLY if critical since the reviser has already made changes based on your previous feedback.
+If you think the article is sufficient or that non critical revisions are required, please aim to return 'NULL'.
+"""
+
+
+def get_review_prompt(state: State) -> PromptTemplate:
+    """Return a prompt for reviewing a draft based on an office template"""
+
+    prompt_template_1 = f"""
+    You have been tasked with reviewing the draft which was written based on a specific template.
+    Send it for revision, along with your notes to guide the revision.
+    {reviser_addendum if state.get('revision_notes') else ""}
+
+    Template: {my_office_template}\nDraft: {{draft}}\n
+    """
+
+    prompt_template_2 = f"""
+    You have been tasked with reviewing the draft which was written based on a specific template.
+    Please accept the draft if it is good enough to publish, or send it for revision, along with your notes to guide the revision.
+    If not all of the template criteria are met, you should send appropriate revision notes.
+    If the draft meets all the guidelines, please return 'NULL'.
+    {reviser_addendum if state.get('revision_notes') else ""}
+
+    Template: {my_office_template}\nDraft: {{draft}}\n
+    """
+
+    if state.get("revision_number", 0) == 0:
+        prompt_template = prompt_template_1  # do at least one revision
+    else:
+        prompt_template = prompt_template_2
+
+    input_variables = ["draft"]
+    if state.get("revision_notes"):
+        input_variables.append("revision_notes")
+
+    return PromptTemplate(template=prompt_template, input_variables=input_variables)
---
+++ lgraph/research_agent/research_agent.py
@@ -0,0 +1,183 @@
+from __future__ import annotations
+
+import ast
+
+from os import system
+from typing import TYPE_CHECKING
+
+import lgraph.research_agent.prompt as pt
+
+from dsp_nesta_brain import logger
+from langchain_core.messages import HumanMessage
+from langchain_core.runnables import RunnablePassthrough
+from langgraph.graph import END
+from langgraph.graph import START
+from langgraph.graph import StateGraph
+from lgraph.graph import State
+from llm.llm import default_llm as llm
+
+
+if TYPE_CHECKING:
+    from langchain_core.prompts import PromptTemplate
+
+
+MAX_REVISIONS = 2
+
+
+# inspiration:
+# https://medium.com/towards-data-science/building-a-research-agent-that-can-write-to-google-docs-part-1-4b49ea05a292
+
+
+class AgentState(State):
+    """
+    A dictionary representing the state of the research agent.
+
+    Attributes:
+        draft (str): The current draft of the research report.
+        critique (str): The critique received for the draft.
+        revision_notes (str): Most recent changes made to the draft
+        revision_number (int): The current revision number of the draft.
+        finalized_state (bool): Indicates whether the report is finalized.
+    """
+
+    draft: str
+    critique: str
+    revision_notes: str
+    revision_number: int
+    finalized_state: bool = False
+
+
+def call_model(state: AgentState, prompt: PromptTemplate) -> AgentState:
+    """Call the LLM to generate a response to a prompt."""
+
+    chain = RunnablePassthrough.assign(request=(lambda x: x["messages"][-1])) | prompt | llm
+    result = chain.invoke(state)
+    return result
+
+
+def write(state: AgentState) -> AgentState:
+    """
+    Write the initial draft of the document.
+
+    Args:
+        state (AgentState): The current state of the research agent.
+
+    Returns:
+        AgentState: The updated state of the research agent.
+    """
+
+    result = call_model(state, pt.write_prompt)
+    state["draft"] = result.content
+    return state
+
+
+def review(state: AgentState) -> AgentState:
+    """Review the draft of the document."""
+
+    result = call_model(state, pt.get_review_prompt(state))
+    if result.content == "NULL":
+        state["finalized_state"] = True
+    else:
+        state["critique"] = result.content
+        logger.info(
+            f"The critique of the draft at revision number {state.get('revision_number',0)} was as follows:\n--------------------------------{state['critique']}\n--------------------------------"  # noqa
+        )
+    return state
+
+
+def revise(state: AgentState) -> AgentState:
+    """Revise the draft of the document."""
+
+    state["revision_number"] = state.get("revision_number", 0) + 1
+    logger.info(f"Undertaking revision number {state['revision_number']}")
+    result = call_model(state, pt.revision_prompt)
+    result = ast.literal_eval(result.content)
+    state["draft"] = result.get("draft")
+    state["revision_notes"] = result.get("revision_notes")
+    return state
+
+
+def terminate(state: AgentState) -> AgentState:
+    """Reject the draft of the document."""
+    logger.info("Draft terminated")
+    return state
+
+
+def accept(state: AgentState) -> AgentState:
+    """Accept the draft of the document."""
+    logger.info("Draft accepted")
+    return state
+
+
+# copied from https://medium.com/towards-data-science/building-a-research-agent-that-can-write-to-google-docs-part-1-4b49ea05a292
+def should_continue(state: AgentState) -> str:
+    """
+    Determine whether the research process should continue based on the current state.
+
+    Args:
+        state (AgentState): The current state of the research agent.
+
+    Returns:
+        str: The next node to transition to ("to_review", "accepted", or "terminated").
+    """
+    # always send to review if editor hasn't made comments yet
+
+    if state.get("finalized_state"):
+        return "accept"
+    elif state.get("revision_number", 0) >= MAX_REVISIONS:
+        logger.info("The number of revisions has reached the maximum ... terminating")
+        return "terminate"
+    else:
+        return "revise"
+
+
+agent = StateGraph(AgentState)
+
+# agent.add_node("initial_plan", nodes.plan_node)
+agent.add_node("write", write)
+agent.add_node("review", review)
+# if False:
+agent.add_node("terminate", terminate)
+agent.add_node("revise", revise)
+agent.add_node("accept", accept)
+
+
+# Edges
+agent.add_edge(START, "write")
+agent.add_edge("write", "review")
+agent.add_edge("revise", "review")
+agent.add_edge("accept", END)
+agent.add_edge("terminate", END)
+agent.add_conditional_edges("review", should_continue)
+
+
+# stream_nodes = ["call_model"]  # list of nodes whose outputs are to be streamed IN ORDER
+
+graph = agent.compile()
+
+
+if __name__ == "__main__":
+
+    toggle = True
+
+    if toggle:
+
+        input = "Write a project proposal for a new project assessing the recent uptake of heat pumps in the UK."
+
+        input = {
+            "messages": [HumanMessage(content=input)],
+            "filter_condition": None,
+            "limit": 10,
+            "use_hybrid_search": True,
+        }
+
+        state = AgentState(input)
+        state = graph.invoke(state)
+
+        for k, v in state.items():
+            print("\n", f"{k}: {v}")  # noqa
+
+    else:
+
+        graph.get_graph().draw_mermaid_png(output_file_path="lgraph/research_agent/mermaid.png")
+        system("open lgraph/research_agent/mermaid.png")  # nosec
