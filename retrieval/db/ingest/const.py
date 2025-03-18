853199fce0d49ef60031f9e62900b274cd5485ab
Helen <helen@climatenode.org>
2025-03-18 09:18:22 +0000

    Edits in response to PR comments

--- retrieval/db/ingest/const.py
+++ retrieval/db/ingest/const.py
@@ -1,5 +1,8 @@
 from typing import Type
 
 
-Chunk: Type = None
-CHUNK_TABLE_NAME: str = None
+# this file contains variables which are dependent on project settings but used in more than one module
+# see #retrieval/db/ingest/nesta_brain.py and retrieval/db/ingest/policy_atlas.py for how they are set
+# also used in retrieval/db/ingest/ingest.py
+Chunk: Type = None  # the schema class to interpret as a "Chunk" in the database
+CHUNK_TABLE_NAME: str = None  # the name of the Lance DB table containing the chunks
