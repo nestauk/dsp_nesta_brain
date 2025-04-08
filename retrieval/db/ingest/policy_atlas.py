ac422e13af56c5961d87229abb61da4964254a0f
Helen <helen@climatenode.org>
2025-04-08 16:11:16 +0100

    Minor tidying

--- retrieval/db/ingest/policy_atlas.py
+++ retrieval/db/ingest/policy_atlas.py
@@ -8,7 +8,7 @@ import retrieval.db.ingest.ingest as ing
 from dsp_nesta_brain import PROJECT_DIR
 from dsp_nesta_brain import logger
 from langchain.docstore.document import Document as LangchainDocument
-from retrieval.db.schema.policy_atlas import Activity
+from retrieval.db.schema.policy_atlas import Activity as Chunk
 
 
 DATA_PATH = PROJECT_DIR / "data/policy_atlas/fcdo_iati_data_2025_01_17.csv"
@@ -16,16 +16,15 @@ DATA_PATH = PROJECT_DIR / "data/policy_atlas/fcdo_iati_data_2025_01_17.csv"
 
 def chunk_already_in_db(chunk: LangchainDocument, **kwargs) -> bool:
     """Determine whether identical chunks have already been added to the database.
-
     Chunking strategy should have been the same.
-    """
+    """  # noqa
 
     where_condition = f'iati_identifier == "{chunk.metadata["iati_identifier"]}"'
     results = ing.chunk_already_in_db(chunk, where_condition=where_condition, **kwargs)
     return bool(results)
 
 
-async def chunk_to_Chunk(chunk: LangchainDocument, ingestion: bool = True) -> Activity:
+async def chunk_to_Chunk(chunk: LangchainDocument, ingestion: bool = True) -> Chunk:
     """
     Convert a Langchain document into an object
     of the Chunk class which can be ingested into the DB
@@ -37,6 +36,15 @@ async def chunk_to_Chunk(chunk: LangchainDocument, ingestion: bool = True) -> Ac
     except Exception as e:
         return e
 
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!
+# documents_to_Chunks
+# AND
+# ingest
+# methods were at one point deleted from this file  (when compared with handover-tidying-0)
+# Assume they are not needed anymore?
+# CHECK THIS MAKES SENSE
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 if __name__ == "__main__":
 
@@ -52,11 +60,8 @@ if __name__ == "__main__":
     )  # the number of CSV rows to ingest at a time. Batch sizes of 230+ seem to get errors back from OpenAI.
 
     # settings constants which may be needed in other files
-    const.Chunk = Activity
+    const.Chunk = Chunk
     const.CHUNK_TABLE_NAME = "activity"
-
-    # global variable
-    request_counter = ing.RequestCounter()
 
     args = parser.parse_args()
 
