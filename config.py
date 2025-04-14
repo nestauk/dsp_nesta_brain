f30895e11fa43cca530dbe63fd1afda17b7ef7bd
Helen <helen@climatenode.org>
2025-04-14 16:28:52 +0100

    Minor edits testing Policy Atlas ingestion

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from typing import Literal
 
 
-PROJECT: Literal["NESTA_BRAIN", "POLICY_ATLAS"] = "NESTA_BRAIN"
+PROJECT: Literal["NESTA_BRAIN", "POLICY_ATLAS"] = "POLICY_ATLAS"
 
 DEFAULT_MODEL: str = "gpt-4o-mini"
 DEFAULT_EMBEDDINGS_MODEL: str = "text-embedding-3-small"
@@ -19,16 +19,16 @@ AZURE_EMBEDDINGS_MODEL: str = DEFAULT_EMBEDDINGS_MODEL
 if PROJECT == "NESTA_BRAIN":
     ALLOW_POLICY_DOCS: bool = True  # set as wanted
     if ALLOW_POLICY_DOCS:
-        DB_PATH: str = "retrieval/db/knowledge_bases/nesta_brain"
+        DB_PATH: str = "retrieval/db/knowledge_bases/test"  # nesta_brain"
     else:
-        DB_PATH: str = "retrieval/db/knowledge_bases/full_site_demo_db_with_pdfs"
+        DB_PATH: str = "retrieval/db/knowledge_bases/test"  # full_site_demo_db_with_pdfs"
     EARLIEST_YEAR: int = 2003  # 2003 is the earliest publication date in the DB
     DEFAULT_START_YEAR: int = 2019
     USE_LANGFUSE: bool = True  # set as wanted
 
 if PROJECT == "POLICY_ATLAS":
     ALLOW_POLICY_DOCS: bool = False  # not relevant to Policy Atlas
-    DB_PATH: str = "retrieval/db/knowledge_bases/policy_atlas"
+    DB_PATH: str = "retrieval/db/knowledge_bases/test"  # policy_atlas"
     EARLIEST_YEAR: int = 1900
     DEFAULT_START_YEAR: int = 2019
     USE_LANGFUSE: bool = False  # Langfuse is not currently set up for other projects –
